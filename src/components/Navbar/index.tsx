d4f6d1dc7bfde190ffec5387b6ac2924e210228f
AhmedAdel1999 <ahmedgithub1999@gmail.com>
2026-01-11 13:33:45 +0200

    fix nav

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,12 +4,11 @@ import NavbarLogo from "./NavbarLogo";
 import DesktopActions from "./DesktopActions";
 import MobileActions from "./MobileActions";
 import LoginButton from "./LoginButton";
-import UserBtn from "./UserBtn";
 
 const Navbar = () => {
   return (
-    <nav className="p-6 rounded-[64px] bg-white">
-      <div className="flex  flex-wrap items-center justify-between gap-4">
+    <nav className="py-6 px-4 rounded-[64px] bg-white">
+      <div className="flex flex-wrap items-center justify-between gap-3">
         {/* Left Section: Logo, Location, Search */}
         <div className="flex items-center grow gap-6">
           <NavbarLogo />
